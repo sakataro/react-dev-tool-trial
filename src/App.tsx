08e944a49103ea6f603aefa2a4b6bed17b24686b
Taro SAKAMOTO <gorgo0013@gmail.com>
2021-12-03 17:51:45 +0900

    演習用のコード追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,62 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App(): JSX.Element {
-  const [count, setCount] = useState(0);
+const createId = (): string => Math.random().toString(32).substring(2);
+const createDefaultSomethings = () => {
+  return [...Array(5000)].map((value, index) => {
+    return {
+      id: createId(),
+      name: `なんかの名前${index}`,
+    };
+  });
+};
 
+function App(): JSX.Element {
+  const [name, setName] = useState('');
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSomethings([
+      ...somethings,
+      {
+        id: createId(),
+        name,
+      },
+    ]);
+    setName('');
+  };
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e);
+  };
+  const [somethings, setSomethings] = useState(createDefaultSomethings());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>なんかのアプリ</h1>
+      <h2>作成</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="taskName"
+          type="text"
+          value={name}
+          onChange={handleChange}
+        />
+        <button type="submit">作成</button>
+      </form>
+      <h2>一覧</h2>
+      <div>
+        {somethings.map((something) => (
+          <Something key={something.id} name={something.name} />
+        ))}
+      </div>
     </div>
   );
 }
+
+const Something = ({ name }: { name: string }): JSX.Element => {
+  return (
+    <div>
+      <p>{name}</p>
+    </div>
+  );
+};
 
 export default App;
